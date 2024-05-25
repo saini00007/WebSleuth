1af13bbcb99911fd2d4b6f354cb988bc073be0dd
saini00007 <115539468+saini00007@users.noreply.github.com>
2024-05-25 15:00:37 +0530

    Update Archive.py

--- SRC/Archive.py
+++ SRC/Archive.py
@@ -5,8 +5,8 @@ from datetime import datetime, timedelta
 from statistics import mean
 
 # ANSI escape codes for colorization
-GREEN = "\033[92m"
-RED = "\033[91m"
+GREEN = "\033[32m"
+RED = "\033[31m"
 BLUE = "\033[34m"
 RESET = "\033[0m"
 
